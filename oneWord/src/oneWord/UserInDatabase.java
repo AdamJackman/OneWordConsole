8ca2e0560a8b186c460a78a70145179975db8d59
AdamJackman <adam.jackman@mail.utoronto.ca>
2015-01-18 21:43:14 -0500

    definitely connected correctly

--- oneWord/src/oneWord/UserInDatabase.java
+++ oneWord/src/oneWord/UserInDatabase.java
@@ -2,16 +2,17 @@ package oneWord;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserInDatabase implements UserDAO {
 	
 	public static boolean DEBUG = true;
+	
+	
 	//log in the user matching the user name and password combination
-	public User login(String username, String password){
-		System.out.println("Test");
-		
-		
+	public User login(String username, String password){	
 		//Make the connection
 		Connection con = null;
 		try{
@@ -33,9 +34,41 @@ public class UserInDatabase implements UserDAO {
 			}
 			con = null;
 		}
+		//Connection created, continue with login
+		
+		
+		//First check if the user is already in the database
+		//SELECT * FROM user WHERE email= email
+		try{
+			String existQuery = "SELECT * FROM users WHERE username=?;";
+			PreparedStatement existPS = con.prepareStatement(existQuery);
+			existPS.setString(1, username);
+			ResultSet rs = existPS.executeQuery();
+			while (rs.next()){
+				//TODO: Change this to a password match
+				System.out.println();
+				System.out.println(rs.getString("username"));
+				if (rs.getString("username").equals(username)){
+					//TODO: change the functionality to return a user
+					con.close();
+					return null; // user must exist, so return null
+				}
+			}
+			rs.close();
+			existPS.close();
+		}
+		catch(SQLException e){
+			if (DEBUG){
+				System.err.println("SQLException: " + e.getMessage());
+			}
+		}
 		
 		
 		
+		
+		
+		//closing the con before the return
+		con = null;
 		return new User("adam", "jackman", 1);
 	}
 	
